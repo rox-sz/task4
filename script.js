4d64548368fe929f7cf9b88aeb1d9024794f88c2
Roxana <roxanaszuciu@gmail.com>
2023-07-12 16:24:05 +0300

    task8

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector('.hamburger')
 const close = document.querySelector('.close-menu')
 const nav2 = document.querySelector('.nav2')
+const projects = document.querySelector('.projects')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.add('hide');
     close.classList.remove('hide');
     nav2.classList.toggle('visible');
+    projects.classList.add('hide');
 
 })
 
